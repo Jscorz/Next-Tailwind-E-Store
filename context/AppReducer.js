154e233a921dcfc5b05af710aece2b3ae0b5b45b
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-31 10:29:22 -0600

    started building context using reducer and useEffect to store items to local storage any time state changes

---
+++ context/AppReducer.js
@@ -0,0 +1,15 @@
+export const initialState = {
+  prices: [],
+  products: {},
+};
+
+export const AppReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "add_product": {
+      // id
+      return {
+        ...state,
+      };
+    }
+  }
+};
