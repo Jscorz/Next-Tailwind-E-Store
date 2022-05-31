56d19dfb510267110f162f7225a09fd613d2d017
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-31 16:33:11 -0600

    mapped over state.products to display cart items

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,7 @@ function Header() {
   const { state, dispatch } = useAppContext();
   const [displayCheckout, setDisplayCheckout] = React.useState(false);
   const modalRef = React.useRef();
+  console.log(state.products);
 
   async function checkout() {
     const lineItems = [
@@ -55,54 +56,17 @@ function Header() {
               </div>
             </div>
             <hr className='py-2' />
-            {Object.keys(state.products).map((productId, index) => {
-              const prod = state.products[productId];
-              console.log(prod);
-              const product = state.prices.find((val) => val.id === productId);
-              return (
-                <div key={index} className='flex flex-col gap-4'>
-                  {Object.keys(prod).map((size) => {
-                    const number = prod[size];
-                    return (
-                      <div
-                        key={size}
-                        className='border-l border-solid border-gray-100 text-xs  p-2 flex flex-col gap-3'
-                      >
-                        <div className='flex items-center justify-between'>
-                          <p className='truncate'>{product.product.name}</p>
-                          <p>${product.unit_amount / 100}</p>
-                        </div>
-                        <div className='font-extralight flex justify-between items-center'>
-                          <h1>SIZE: {size}</h1>
-                          <div>
-                            <h1>
-                              QUANTITY:{" "}
-                              <span className='pl-4 border border-solid py-1 pr-6 border-gray-400 ml-3 relative'>
-                                {number}
-                                <div className='absolute top-0 right-0 h-full w-3 flex flex-col '>
-                                  <div
-                                    className='leading-none scale-75 cursor-pointer'
-                                    onClick={increment(productId, size, number)}
-                                  >
-                                    <i className='fa-solid fa-chevron-up'></i>
-                                  </div>
-                                  <div
-                                    className='leading-none scale-75 cursor-pointer'
-                                    onClick={decrement(productId, size, number)}
-                                  >
-                                    <i className='fa-solid fa-chevron-down'></i>
-                                  </div>
-                                </div>
-                              </span>
-                            </h1>
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
+            {Object.keys(
+              state.products.map((productId, index) => {
+                console.log(product);
+                const prod = state.products[productId];
+                const product = state.prices.find(
+                  (val) => val.id === productId
+                );
+                console.log(product);
+                return <div key={index} className='h2 w-2 bg-green-200'></div>;
+              })
+            )}
           </div>
           <button
             onClick={checkout}
