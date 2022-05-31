3af3410daf646bd224d118a7aa52917abfb4bdc1
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-31 10:19:15 -0600

    added instagram logo to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className='grid place-items-center py-4'>
+      <a href='#'>
+        <i className='fa-brands fa-instagram transition hover:text-green-400 cursor pointer'></i>
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
