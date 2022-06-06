7c3e44a9ab7c10de7c5e4d91f4827c8b16374d5b
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-06 12:11:21 -0600

    updated layout of purchase cards

--- components/Layout.js
+++ components/Layout.js
@@ -3,18 +3,18 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 function Layout(props) {
-  const { children } = props;
-  return (
-    <div className='relative min-h-screen flex flex-col'>
-      <Header />
-      <div className='flex flex-col flex-1'>
-        <main className='flex-1 py-5 sm:max-w-2/3 mx-auto flex-wrap gap-5'>
-          {children}
-        </main>
-        <Footer />
-      </div>
-    </div>
-  );
+	const { children } = props;
+	return (
+		<div className='relative min-h-screen flex flex-col '>
+			<Header />
+			<div className='flex flex-col flex-1'>
+				<main className='flex-1 py-5 sm:max-w-2/3 mx-auto flex-wrap '>
+					{children}
+				</main>
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default Layout;
--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -2,56 +2,56 @@ import React from "react";
 import { useAppContext } from "../../context/CardContext";
 
 export async function getServerSideProps(context) {
-  const { params } = context;
-  return {
-    props: { id: params.id },
-  };
+	const { params } = context;
+	return {
+		props: { id: params.id },
+	};
 }
 
 function Description(props) {
-  const { id: path } = props;
-  const { state: { prices } = [], dispatch } = useAppContext();
-  const product = prices.filter((val) => val.id === path.replace("/", ""))[0];
+	const { id: path } = props;
+	const { state: { prices } = [], dispatch } = useAppContext();
+	const product = prices.filter((val) => val.id === path.replace("/", ""))[0];
 
-  function addToBasket(prod) {
-    return () => {
-      dispatch({
-        type: "add_product",
-        value: prod.id,
-      });
-    };
-  }
+	function addToBasket(prod) {
+		return () => {
+			dispatch({
+				type: "add_product",
+				value: prod.id,
+			});
+		};
+	}
 
-  if (!product) {
-    return <div>no product</div>;
-  }
-  return (
-    <div className='mx-auto w-fit flex flex-wrap justify-center md:gap-6'>
-      <img
-        src={product.product.images[0]}
-        alt={product.product.id}
-        style={{ maxHeight: "600px" }}
-      />
-      <div className=''>
-        <div className='flex justify-between items-center'>
-          <h1 className='text-sm py-3 font-light tracking-wide capitalize text-2xl lg:text-3xl'>
-            {product.product.name}
-          </h1>
-          <p>${product.unit_amount / 100}</p>
-        </div>
-        <div className='max-w-full whitespace-normal text-jusitfy pb-4 font-light'>
-          <p>{product.product.description}</p>
-        </div>
-        <hr />
-        <button
-          onClick={addToBasket(product)}
-          className='w-full my-4 py-4 border border-solid border-gray-100 shadow bg-slate-100 text-slate-700 font-light transition duration-300 hover:opacity-50'
-        >
-          ADD TO BASKET
-        </button>
-      </div>
-    </div>
-  );
+	if (!product) {
+		return <div>no product</div>;
+	}
+	return (
+		<div className='mx-auto w-fit flex flex-wrap justify-center md:gap-6'>
+			<img
+				src={product.product.images[0]}
+				alt={product.product.id}
+				style={{ maxHeight: "600px" }}
+			/>
+			<div className=''>
+				<div className='flex justify-between items-center pb-8'>
+					<h1 className='text-sm py-3 font-light tracking-wide capitalize text-2xl lg:text-3xl'>
+						{product.product.name}
+					</h1>
+					<p>${product.unit_amount / 100}</p>
+				</div>
+				<div className='max-w-full whitespace-normal text-jusitfy pb-4 font-light'>
+					<p>{product.product.description}</p>
+				</div>
+				<hr />
+				<button
+					onClick={addToBasket(product)}
+					className='w-full my-4 py-4 border border-solid border-gray-100 shadow bg-slate-100 text-slate-700 font-light transition duration-300 hover:opacity-50'
+				>
+					ADD TO BASKET
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Description;
