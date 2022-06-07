82b752ee5a0b329cbdb22e6dfdd09b5bc871bb4b
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-07 10:45:33 -0600

    adjusted copyright symbol

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,7 @@ import React from "react";
 function Footer() {
 	return (
 		<div className='grid place-items-center py-4'>
-			<a href='#'>
-				<i className='fas fa-copyright transition hover:text-green-400 cursor pointer'></i>
-			</a>
+			<i className='fas fa-copyright transition hover:text-green-400'></i>
 		</div>
 	);
 }
--- components/HeroTwo.js
+++ components/HeroTwo.js
@@ -13,10 +13,11 @@ function HeroTwo() {
 				layout='fill'
 				className='object-cover z-0'
 			/>
+			<div class='absolute top-0 bottom-0 right-0 left-0 bg-gradient-to-b from-transparent to-gray-900 '></div>
 			<div className='min-w-full flex flex-col z-10 absolute text-center top-1/3'>
 				<div className='w-30 space-y-8'>
 					<h2 className='text-white '>Sale up to 50% Off</h2>
-					<h1 className='uppercase text-3xl text-white text-bold tracking-widest'>
+					<h1 className='uppercase text-2xl text-white text-bold tracking-widest md:text-3xl'>
 						Blue / Black Nike
 					</h1>
 
@@ -25,12 +26,12 @@ function HeroTwo() {
 					</button>
 				</div>
 			</div>
-			<button className='absolute z-10 bg-white text-black text-lg bottom-[55%] left-10'>
+			{/* <button className='absolute z-10 bg-white text-black text-lg bottom-[55%] left-10'>
 				<i className='fa-solid fa-angle-left'></i>
 			</button>
 			<button className='absolute z-10 bg-white text-black text-lg bottom-[55%] right-10'>
 				<i className='fa-solid fa-angle-right'></i>
-			</button>
+			</button> */}
 		</div>
 	);
 }
