dbc2bc06e8342720b86728cc91c7120fad692b7f
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-08 11:03:08 -0600

    configured envs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+	env: {
+		STRIPE_PUBLIC: process.env.STRIPE_PUBLIC,
+		STRIPE_SECRET: process.env.STRIPE_SECRET,
+	},
+};
