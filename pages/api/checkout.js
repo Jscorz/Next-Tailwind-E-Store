536d8e044407fa9fbb7a2ae971e79cdffcd0f9f1
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-09 08:45:42 -0600

    minified commented code

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,32 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import Stripe from "stripe";
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "POST method required" });
-  }
-  const body = JSON.parse(req.body);
+	if (req.method !== "POST") {
+		return res.status(405).json({ message: "POST method required" });
+	}
+	const body = JSON.parse(req.body);
 
-  if (body.lineItems.length === 0) {
-    return res
-      .status(405)
-      .json({ message: "Please select items for checkout" });
-  }
+	if (body.lineItems.length === 0) {
+		return res
+			.status(405)
+			.json({ message: "Please select items for checkout" });
+	}
 
-  try {
-    const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
-      apiVersion: "2020-08-27",
-    });
-    const session = await stripe.checkout.sessions.create({
-      success_url: "http://localhost:3000/success",
-      cancel_url: "http://localhost:3000/cancel",
-      line_items: body.lineItems,
-      mode: "payment",
-    });
+	try {
+		const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
+			apiVersion: "2020-08-27",
+		});
+		const session = await stripe.checkout.sessions.create({
+			success_url: "http://localhost:3000/success",
+			cancel_url: "http://localhost:3000/cancel",
+			line_items: body.lineItems,
+			mode: "payment",
+		});
 
-    res.status(201).json({ session });
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
+		res.status(201).json({ session });
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
 }
