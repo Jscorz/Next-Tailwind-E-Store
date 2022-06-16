1456d4d16b8e257e585ab05031750b211d27b4af
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-16 15:51:34 -0600

    udpated class to className

--- components/Footer.js
+++ components/Footer.js
@@ -4,88 +4,88 @@ import { MdOutlineMail } from "react-icons/md";
 import { AiOutlinePhone } from "react-icons/ai";
 function Footer() {
 	return (
-		<footer class='bg-darkBlue2 text-slate-900'>
-			<div class='container mx-auto pt-12 px-5 pb-10'>
-				<div class='flex flex-col justify-between space-y-24 md:flex-row md:space-y-0'>
-					<div class='mt-10 space-y-6'>
-						<div class='flex items-center space-x-3 md:-mt-10'>
-							<div class='w-6'>
-								<AiOutlinePhone class='scale-10 text-2xl' />
+		<footer className='bg-darkBlue2 text-slate-900'>
+			<div className='container mx-auto pt-12 px-5 pb-10'>
+				<div className='flex flex-col justify-between space-y-24 md:flex-row md:space-y-0'>
+					<div className='mt-10 space-y-6'>
+						<div className='flex items-center space-x-3 md:-mt-10'>
+							<div className='w-6'>
+								<AiOutlinePhone className='scale-10 text-2xl' />
 							</div>
 							<div>+1-533-489-3980</div>
 						</div>
 
-						<div class='flex items-center space-x-3'>
-							<div class='w-6'>
-								<MdOutlineMail class='scale-10 text-2xl' />
+						<div className='flex items-center space-x-3'>
+							<div className='w-6'>
+								<MdOutlineMail className='scale-10 text-2xl' />
 							</div>
 							<div>example@shoesinstock.com</div>
 						</div>
 					</div>
 
-					<div class='flex flex-col space-y-10 text-xl md:text-base md:space-x-20 md:space-y-0 md:flex-row'>
-						<div class='flex flex-col space-y-3'>
+					<div className='flex flex-col space-y-10 text-xl md:text-base md:space-x-20 md:space-y-0 md:flex-row'>
+						<div className='flex flex-col space-y-3'>
 							<a
 								href='#'
-								class='transition duration-300 hover:text-teal-400'
+								className='transition duration-300 hover:text-teal-400'
 							>
 								About
 							</a>
 							<a
 								href='#'
-								class='transition duration-300 hover:text-teal-400'
+								className='transition duration-300 hover:text-teal-400'
 							>
 								Jobs
 							</a>
 							<a
 								href='#'
-								class='transition duration-300 hover:text-teal-400'
+								className='transition duration-300 hover:text-teal-400'
 							>
 								Press
 							</a>
 							<a
 								href='#'
-								class='transition duration-300 hover:text-teal-400'
+								className='transition duration-300 hover:text-teal-400'
 							>
 								Blog
 							</a>
 						</div>
-						<div class='flex flex-col space-y-3'>
+						<div className='flex flex-col space-y-3'>
 							<a
 								href='#'
-								class='transition duration-300 hover:text-teal-400'
+								className='transition duration-300 hover:text-teal-400'
 							>
 								Contact Us
 							</a>
 							<a
 								href='#'
-								class='transition duration-300 hover:text-teal-400'
+								className='transition duration-300 hover:text-teal-400'
 							>
 								Terms
 							</a>
 							<a
 								href='#'
-								class='transition duration-300 hover:text-teal-400'
+								className='transition duration-300 hover:text-teal-400'
 							>
 								Privacy
 							</a>
 						</div>
 					</div>
 
-					<div class='flex justify-center pb-10 space-x-3'>
-						<div className='w-10 h-20'>
+					<div className='flex justify-center pb-10 space-x-3'>
+						<div classNameName='w-10 h-20'>
 							<a href='#'>
-								<BsFacebook class='p-2 rounded-full ficon text-blue-900 w-10 h-10 ' />
+								<BsFacebook className='p-2 rounded-full ficon text-blue-900 w-10 h-10 ' />
 							</a>
 						</div>
-						<div className='w-10 h-20'>
+						<div classNameName='w-10 h-20'>
 							<a href='#'>
-								<BsTwitter class='p-2 rounded-full ficon text-blue-900 w-10 h-10 ' />
+								<BsTwitter className='p-2 rounded-full ficon text-blue-900 w-10 h-10 ' />
 							</a>
 						</div>
-						<div className='w-10 h-20'>
+						<div classNameName='w-10 h-20'>
 							<a href='#'>
-								<BsInstagram class='p-2  ficon w-10 h-10 text-blue-900' />
+								<BsInstagram className='p-2  ficon w-10 h-10 text-blue-900' />
 							</a>
 						</div>
 					</div>
