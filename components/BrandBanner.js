38e59daaef94aa11949a8abaaa25b56579aa7c5c
Justin Scorzafava <jkscorzafava@gmail.com>
2022-08-10 12:36:52 -0600

    added Router.push to push to nike page

--- components/BrandBanner.js
+++ components/BrandBanner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Router from "next/router";
 import Image from "next/image";
 import drMartensLogo from "../images/drmartens-logo.jpg";
 import vansLogo from "../images/vans-logo.jpg";
@@ -15,7 +16,7 @@ const BrandBanner = () => {
 			<div className=' h-3/4 w-11/12 mx-auto pt-10 text-black z-30 relative grid grid-cols-12 grid-rows-6 gap-5'>
 				<div className='border-2  border-slate-700 bg-gray-300 col-start-1 col-end-7 row-start-1 row-span-6  overflow-hidden rounded-md relative group  md:col-end-6 md:row-start-1 md:row-span-6'>
 					<button className='hidden top-0 bottom-0 right-0 left-0 w-full bg-gradient-to-t from-transparent to-white group-hover:absolute group-hover:block z-50 relative '>
-						<h1 className='text-center text-2xl uppercase  text-bolder py-8 tracking-wider md:text-3xl'>
+						<h1 className='text-center text-2xl uppercase text-slate-700 text-bolder py-8 tracking-wider md:text-3xl'>
 							Dr. Martens
 						</h1>
 					</button>
@@ -29,7 +30,7 @@ const BrandBanner = () => {
 					/>
 				</div>
 				<div className='border-2  border-slate-700 bg-gray-300 col-start-7 col-end-13 row-start-1 row-end-3 overflow-hidden rounded-md relative group md:col-start-6 md:col-end-9 md:row-start-1 md:row-end-5'>
-					<button className='hidden top-0 bottom-0 right-0 left-0 w-full bg-gradient-to-t from-transparent to-white group-hover:absolute group-hover:block z-50 text-center uppercase text-2xl text-bolder py-8 tracking-wider md:text-3xl'>
+					<button className='hidden top-0 bottom-0 right-0 left-0 w-full bg-gradient-to-t from-transparent to-white group-hover:absolute group-hover:block z-50 text-center uppercase text-2xl text-slate-700 text-bolder py-8 tracking-wider md:text-3xl'>
 						Vans
 					</button>
 					<Image
@@ -42,7 +43,7 @@ const BrandBanner = () => {
 					/>
 				</div>
 				<div className='border-2  border-slate-700 bg-gray-300 col-start-7 col-end-13  row-start-3 row-end-4 overflow-hidden rounded-md  relative group md:col-start-6 md:col-end-9  md:row-start-5 md:row-end-7'>
-					<button className='hidden top-0 bottom-0 right-0 left-0 w-full bg-gradient-to-t from-transparent to-white group-hover:absolute group-hover:block z-50 text-center uppercase text-2xl text-bolder py-8 tracking-wider md:text-3xl'>
+					<button className='hidden top-0 bottom-0 right-0 left-0 w-full bg-gradient-to-t from-transparent to-white group-hover:absolute group-hover:block z-50 text-center uppercase text-2xl text-slate-700 text-bolder py-8 tracking-wider md:text-3xl'>
 						Converse All Star
 					</button>
 					<Image
@@ -55,7 +56,7 @@ const BrandBanner = () => {
 					/>
 				</div>
 				<div className='border-2  border-slate-700 bg-gray-300 col-start-7 col-end-13 row-start-4 row-end-6 overflow-hidden rounded-md  relative group md:col-start-9 md:col-end-13 md:row-start-1 md:row-end-4'>
-					<button className='hidden top-0 bottom-0 right-0 left-0 w-full bg-gradient-to-t from-transparent to-white group-hover:absolute group-hover:block z-50 text-center uppercase text-2xl text-bolder py-8 tracking-wider md:text-3xl'>
+					<button className='hidden top-0 bottom-0 right-0 left-0 w-full bg-gradient-to-t from-transparent to-white group-hover:absolute group-hover:block z-50 text-center uppercase text-2xl text-slate-700 text-bolder py-8 tracking-wider md:text-3xl'>
 						Adidas
 					</button>
 					<Image
@@ -68,7 +69,10 @@ const BrandBanner = () => {
 					/>
 				</div>
 				<div className='border-2  border-slate-700 bg-gray-300 col-start-7 col-end-13 row-start-6 row-end-7 overflow-hidden rounded-md  relative group md:col-start-9 md:col-end-13 md:row-start-4 md:row-end-7'>
-					<button className='hidden top-0 bottom-0 right-0 left-0 w-full bg-gradient-to-t from-transparent to-white group-hover:absolute group-hover:block z-50 text-center uppercase text-2xl text-bolder py-8 tracking-wider md:text-3xl'>
+					<button
+						onClick={() => Router.push("/nike")}
+						className='hidden top-0 bottom-0 right-0 left-0 w-full bg-gradient-to-t from-transparent to-white group-hover:absolute group-hover:block z-50 text-center uppercase text-2xl text-slate-700 text-bolder py-8 tracking-wider md:text-3xl'
+					>
 						Nike
 					</button>
 					<Image
